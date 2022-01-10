66e36b227d6267a368c8c3fd2cb9e77f1b866382
Carolyn Ponce <35043294+carolynponce@users.noreply.github.com>
2022-01-10 15:38:33 -0600

    Update ActivityAccumulatorMinimumTimestamp.sql

--- SaaS/ActivityAccumulatorMinimumTimestamp.sql
+++ SaaS/ActivityAccumulatorMinimumTimestamp.sql
@@ -1,5 +1,5 @@
--- Activity Accumulator Minimum Time Stamp
--- SaaS Version
+-- Title: Activity Accumulator Minimum Time Stamp
+-- Version: SaaS
 -- Description: Finds the time and date of the oldest activity in the Activitiy Accumulator.
 
 Select MIN(timestamp) from activity_accumulator
