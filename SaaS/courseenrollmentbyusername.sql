dd0215384ecb73212378b817fc5ae193a2e76afa
Carolyn Ponce <35043294+carolynponce@users.noreply.github.com>
2022-01-10 14:02:25 -0600

    Create courseenrollmentbyusername.sql

---
+++ SaaS/courseenrollmentbyusername.sql
@@ -0,0 +1,21 @@
+-- Chris Baca, SAGU
+
+select
+cu.pk1
+,concat(u.lastname, ', ', u.firstname) as student_name
+,u.email as student_email
+,cm.course_id
+from course_users cu
+	inner join users u on u.pk1 = cu.users_pk1
+	inner join course_main cm on cm.pk1 = cu.crsmain_pk1
+	inner join course_term ct on ct.crsmain_pk1 = cm.pk1
+	inner join term t on t.pk1 = ct.term_pk1
+where cu.role = 'S'
+	and cu.row_status = '0'
+	and u.user_id like 'InsertUsername'
+	and cm.pk1 not in (
+						select
+							crsmain_pk1
+						from course_course
+						)
+order by course_id, student_name
